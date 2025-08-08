0a57e59c259f3e229952901d34cd7a41ca5270d7
jhordanMC <diego_mcj05@hotmail.com>
2025-08-07 22:08:05 -0500

    app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/proxy', methods=['GET', 'POST'])
+@app.route('/api', methods=['POST'])
 def proxy():
-    target_url = "http://35.225.198.236:5000/endpoint"  # Ajusta si tu ruta es distinta
-    if request.method == 'GET':
-        response = requests.get(target_url, params=request.args)
-    else:
+    target_url = "http://35.225.198.236:5000/generar_cliente"
+    try:
         response = requests.post(target_url, json=request.get_json())
-    return jsonify(response.json())
+        return jsonify(response.json()), response.status_code
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run()
